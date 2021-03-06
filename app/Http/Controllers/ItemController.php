6af6b856cf84e95c5402df89a557e132bed07c01
boolfalse <boolfalse@mail.ru>
2021-03-07 02:36:11 +0400

    Items CRUD using REST via ItemController

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    public function index()
+    {
+        $item = Item::orderBy('created_at', 'DESC')->get();
+
+        if ($item) {
+            return response()->json([
+                'success' => true,
+                'message' => "Successfully retrieved.",
+                'data' => $item,
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => "Not found!",
+                'data' => [],
+            ], 404);
+        }
+
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        $item = Item::create([
+            'title' => $request->get('title'),
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => "Successfully created.",
+            'data' => $item,
+        ], 201);
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        $item = Item::find($id);
+
+        if ($item) {
+            $completed = $request->get('completed');
+            $update_data = [
+                'completed' => $completed,
+                'completed_at' => $completed ? Carbon::now() : null,
+            ];
+
+            $item->update($update_data);
+
+            return response()->json([
+                'success' => true,
+                'message' => "Successfully updated.",
+                'data' => $item,
+            ], 201);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => "Not found!",
+                'data' => [],
+            ], 404);
+        }
+    }
+
+    public function destroy($id)
+    {
+        $item = Item::find($id);
+
+        if ($item) {
+            $item->delete();
+
+            return response()->json([
+                'success' => true,
+                'message' => "Successfully deleted.",
+                'data' => [],
+            ], 201);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => "Not found!",
+                'data' => [],
+            ], 404);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([
+    'prefix' => 'items',
+], function () {
+    Route::get('/', 'ItemController@index');
+    Route::post('/', 'ItemController@store');
+    Route::put('/{id}', 'ItemController@update');
+    Route::delete('/{id}', 'ItemController@destroy');
 });
