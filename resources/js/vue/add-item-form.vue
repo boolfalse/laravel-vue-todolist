8725b911408a00a86bf3cd226edc3863350373c9
boolfalse <boolfalse@mail.ru>
2021-03-07 13:20:54 +0400

    Create item via Axios

--- resources/js/vue/add-item-form.vue
+++ resources/js/vue/add-item-form.vue
@@ -1,13 +1,69 @@
 
 <template>
-    <div>
-        <input type="text">
+    <div class="add-item">
+        <input type="text" v-model="item.title">
         <font-awesome-icon
             icon="plus-square"
+            @click="addItem()"
+            :class="[ item.title ? 'active' : 'inactive', 'plus', 'cursor-pointer' ]"
         />
     </div>
 </template>
 
 <script>
-    export default {};
+    export default {
+        data: () => {
+            return {
+                item: {
+                    title: "Test todolist item"
+                }
+            }
+        },
+        methods: {
+            addItem() {
+                if (this.item) {
+                    let title = this.item.title;
+
+                    axios.post('/api/items', {
+                        title: title
+                    }).then((res) => {
+                        if (res.success) {
+                            this.item.title = '';
+                        }
+                    }).catch((err) => {
+                        console.log(err);
+                    });
+                }
+            }
+        }
+    };
 </script>
+
+<style>
+.add-item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+input {
+    background: #f7f7f7;
+    border: 0;
+    outline: none;
+    padding: 5px;
+    margin-right: 10px;
+    width: 100%;
+}
+
+.plus {
+    font-size: 20px;
+}
+.active {
+    color: #00ce25
+}
+.active {
+    color: #999999;
+}
+.cursor-pointer {
+    cursor: pointer;
+}
+</style>
